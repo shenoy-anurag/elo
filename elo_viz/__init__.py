39011914f9f95a99d49bd0c6e61ef73e6fe6ce06
Anurag <19914954+shenoy-anurag@users.noreply.github.com>
2025-02-05 19:02:12 -0500

    feat,chore: added Elo and EloChess to __init__.py for easier imports, updated version number and pypi classifiers

--- elo_viz/__init__.py
+++ elo_viz/__init__.py
@@ -1,6 +1,7 @@
-from .matches import Match
-from .player import Player
-from .rating import Rating, calculate_rating
 from .constants import (
     DEFAULT_RATING, RESULT_WIN, RESULT_LOSS, RESULT_DRAW
 )
+from .matches import Match
+from .player import Player
+from .rating import Rating, calculate_rating
+from .elo import Elo, EloChess
